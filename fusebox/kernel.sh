fe1359a280ed6bc98490072071c1c2f18749ab1d
dfuse06 <dfuse06@gmail.com>
2017-11-17 22:37:09 -0600

    first commit

---
+++ fusebox/kernel.sh
@@ -0,0 +1,22 @@
+#!/bin/sh 
+
+CL_RED="\033[31m"
+
+echo "About to make this mother fucking fusebox!!!!!";
+
+secs=$((5 * 1))
+while [ $secs -gt 0 ]; do
+   echo -ne "$secs\033[0K\r"
+   sleep 0.5
+   : $((secs--))
+done
+
+echo -e ${CL_RED}"Fusebox kernel initiating${CL_RED}";
+
+cd ~/fusebox/
+make clean && make mrproper
+export CROSS_COMPILE=/home/dfuse/ndk/toolchains/arm-eabi-5.3/bin/arm-eabi-
+make fusebox_defconfig ARCH=arm
+make -j8 ARCH=arm
+
+done
