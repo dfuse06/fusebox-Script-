3521a82f9e77252bd3972bb278dc884993ef16f6
dfuse06 <dfuse06@gmail.com>
2017-11-19 13:23:44 -0600

    Add commands for lazy zip and cp files over there. Fuck, I am lazy!!!!!!!

--- kernel.sh
+++ kernel.sh
@@ -18,3 +18,30 @@ make clean && make mrproper
 export CROSS_COMPILE=~/ndk/toolchains/arm-eabi-5.3/bin/arm-eabi-
 make fusebox_defconfig ARCH=arm
 make -j8 ARCH=arm
+
+cd ~/lazy/lazy/
+rm -fr zImage-dtb
+rm -fr kernel-flasher-5.1-*
+rm -fr modules/*
+
+cp ~/fusebox/crypto/ansi_cprng.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/char/rdbg.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/input/evbug.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/media/usb/gspca/gspca_main.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/mmc/card/mmc_test.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/spi/spidev.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/usb/gadget/legacy/g_laf.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/video/backlight/backlight.ko ~/lazy/lazy/modules/
+cp ~/fusebox/drivers/video/backlight/lcd.ko ~/lazy/lazy/modules/
+cp ~/fusebox/arch/arm/boot/zImage-dtb ~/lazy/lazy/
+
+echo "Make this zip, BITCH!"
+
+make 
+
+cd ~/
+
+echo "Done mother fucker!" 
+sleep 5
+
+exit 
